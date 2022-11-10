fff71850fa1c962aa6a7175cbd34c57788e6a974
MrDrHax <alexfh2001@gmail.com>
2022-11-10 07:22:18 -0600

    comentarios

--- guillermo.py
+++ guillermo.py
@@ -3,6 +3,9 @@ import random
 color_list = [0,1,2,3]
 GameOrder = []
 
+'''
+Pone en una lista el orden del nivel
+'''
 def Return_GameOrder (level):
     #Returns a list of the level
     global GameOrder
